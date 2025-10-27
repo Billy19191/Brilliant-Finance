f03e26d48a40a05dbffa1685558be608e056b4e7
Billy19191 <bill.ratchanon@gmail.com>
2025-10-27 23:58:47 +0700

    ✨  feat: transaction section

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import ActiveContractsSection from '@/components/dashboard/ActiveContractsSection'
 import OverviewCard from '@/components/dashboard/OverviewCard'
 import OverviewSection from '@/components/dashboard/OverviewSection'
+import TransactionSection from '@/components/dashboard/TransactionSection'
 
 export default function Dashboard() {
   return (
     <div className="px-20 font-inter animate-fade-in">
       <OverviewSection />
       <ActiveContractsSection />
+      <TransactionSection />
     </div>
   )
 }
---
+++ src/components/dashboard/TransactionSection.tsx
@@ -0,0 +1,16 @@
+export default function TransactionSection() {
+  return (
+    <section className="font-inter">
+      <h2 className="text-2xl font-bold">Transaction Activities</h2>
+      <p className="mt-2 text-base">
+        Your recent lending and borrowing transactions are displayed below.
+      </p>
+      <div className="py-8"></div>
+      {/* <div className="mt-6">
+          <p className="text-gray-600">
+            You have no active contracts at the moment.
+          </p>
+        </div> */}
+    </section>
+  )
+}
